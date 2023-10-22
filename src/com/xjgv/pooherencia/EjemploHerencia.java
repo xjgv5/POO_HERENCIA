a0b57f7bd5c24a0de8194851d6d9bef0a7726e3a
Javier Gomez <javier.gov@outlook.es>
2023-10-22 10:43:02 -0600

    Herencia

--- src/com/xjgv/pooherencia/EjemploHerencia.java
+++ src/com/xjgv/pooherencia/EjemploHerencia.java
@@ -5,6 +5,22 @@ public class EjemploHerencia {
         Alumno alumno = new Alumno();
         alumno.setNombre("Andres");
         alumno.setApellido("Guzman");
+        alumno.setInstitucion("Intituto Nacional");
+        alumno.setNotaMatematicas(7);
+        alumno.setNotaLenguaje(8);
+        alumno.setNotaCiencias(8);
+
+        AlumnoInternacional alumnoInt = new AlumnoInternacional();
+        alumnoInt.setNombre("Peter");
+        alumnoInt.setApellido("Doe");
+        alumnoInt.setPais("Australia");
+        alumnoInt.setEdad(16);
+        alumnoInt.setInstitucion("Instituto Nacional");
+        alumnoInt.setNotaIdiomas(9.8);
+        alumnoInt.setNotaMatematicas(9);
+        alumnoInt.setNotaLenguaje(7);
+        alumnoInt.setNotaCiencias(10);
+
         Profesor profesor = new Profesor();
         profesor.setNombre("Luci");
         profesor.setApellido("Pérez");
@@ -16,5 +32,14 @@ public class EjemploHerencia {
         System.out.println("Profesor " + profesor.getAsignatura() +
                 " : " + profesor.getNombre() + " " +
                 profesor.getApellido());
+
+        Class claseAlumnoInt = alumnoInt.getClass();
+        while (claseAlumnoInt.getSuperclass() != null){
+            String hija = claseAlumnoInt.getName();
+            String padre = claseAlumnoInt.getSuperclass().getName();
+            System.out.println(hija + " es una clase hija de la clase padre : " + padre);
+            claseAlumnoInt = claseAlumnoInt.getSuperclass();
+
+        }
     }
 }
